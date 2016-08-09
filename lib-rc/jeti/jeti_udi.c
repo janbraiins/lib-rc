eab87f2d816be75bb0a15c440abef11f36aeb4d8
Jan Čapek <jan.capek@braiins.cz>
2016-08-09 05:24:24 +0000

    JETI UDI protocol support: add initial implementation - extend Kconfig with LIB_RC_JETI option - update top level SConscript

---
+++ lib-rc/jeti/jeti_udi.c
@@ -0,0 +1,218 @@
+/**
+ * @file jeti_udi.c
+ * @author Copyright (c) 2016 Braiins Systems s.r.o. All rights reserved.
+ * @brief JETI UDI12/16 RC channel data parser
+ */
+/* C library includes*/
+#include <string.h>
+
+#include <lib-rc/jeti/jeti_udi.h>
+
+/* FreeRTOS includes */
+#include <FreeRTOS.h>
+#include <task.h>
+
+/* lib RTOS extensions includes */
+#include <lib-rtos/error.h>
+#include <lib-rtos/byteorder/byteorder.h>
+
+/* lib-cobjs includes */
+#include <lib-cobjs/cobjs.h>
+
+/** @addtogroup group_jeti_udi
+ *  @{
+ */
+/**
+ * Baudrate that JETI receivers use for UDI12/16 protocol frames
+ */
+#define JETI_UDI__BAUD 115200
+
+
+#define JETI_UDI__MAX_CHANNELS JETI_UDI__16
+
+/** The queue for storing servo positions is 1 element deep - this is
+ * sufficient since no buffering is needed. Any servo position frames,
+ * that won't fit into the queue, will get dropped. */
+#define JETI_UDI__SERVO_CHANNELS_QUEUE_LENGTH 1
+/** @} */
+
+#if (CONFIG_LIB_RC_RC_CHANNELS_MAX_COUNT < jeti_udi__16)
+#error The number of configured RC channels (CONFIG_LIB_RC_RC_CHANNELS_MAX_COUNT) is not sufficient for Royal Evo9 support!
+#endif
+
+/**
+ * Channel information frame as sent by the receiver in UDI12
+ *
+ * @ingroup group_jeti_udi
+ */
+#if 0
+struct jeti_udi__12_channel_info {
+  union {
+    uint8_t payload[25];
+    struct {
+      /** UDI12 uses 0xA1, UDI16 uses 0xA2 */
+      uint8_t manufacturer_id;
+      /* servo positions in big endian byte order
+       * code */
+      uint16_t channels[12];
+    } s __attribute__ ((packed));
+  } u;
+  /** CRC channels */
+  uint16_t crc16;
+} __attribute__ ((packed));
+#endif
+
+
+struct jeti_udi__12_channel_info {
+  union {
+    uint8_t payload[25];
+    struct {
+      uint8_t manufacturer_id;
+      union {
+        uint8_t payload[24];
+        uint16_t channels[12];
+      } u __attribute__ ((packed));
+    } s;
+  } u1;
+  uint16_t crc16;
+} __attribute__ ((packed));
+
+
+/**
+ * Channel information frame as sent by the receiver in UDI16
+ *
+ * @ingroup group_jeti_udi
+ */
+struct jeti_udi__16_channel_info {
+  /** UDI16 uses 0xA2 */
+  uint8_t manufacturer_id;
+  /* servo positions in big endian byte order
+   * code */
+  uint16_t channels[16];
+  /** CRC channels */
+  uint16_t crc16;
+} __attribute__ ((packed));
+
+
+
+
+struct jeti_udi* jeti_udi__new(uart__id_t uart_id,
+                               jeti_udi__format_t udi_format)
+{
+  COBJS__STD_NEW_BODY(jeti_udi, uart_id, udi_format);
+}
+
+
+int jeti_udi__init(struct jeti_udi *self, uart__id_t uart_id,
+                   jeti_udi__format_t udi_format)
+{
+  int retval = E_OK;
+
+  self->servo_channels_queue = xQueueCreate(JETI_UDI__SERVO_CHANNELS_QUEUE_LENGTH,
+                                            (unsigned portBASE_TYPE)
+                                              sizeof(struct rc_channels));
+
+  if (self->servo_channels_queue == NULL) {
+    retval = E_NO_MEM;
+    goto init_failed;
+  }
+
+  jeti_udi__reset_drop_count(self);
+
+
+  self->uart = uart__new(uart_id,
+                         JETI_UDI__BAUD,
+                         sizeof(struct jeti_udi__12_channel_info),
+                         portMAX_DELAY,
+                         portMAX_DELAY);
+  if (self->uart == NULL) {
+    retval = E_FAIL;
+    goto init_failed;
+  }
+
+ init_failed:
+  return retval;
+}
+
+
+void jeti_udi__rx_task(struct jeti_udi *self)
+{
+  /* storage for the channel information in MPX native format */
+  struct jeti_udi__12_channel_info channel_info;
+  /* normalized values of all servo positions that will get sent for
+   * further processing */
+  struct rc_channels servo_positions;
+  int i, j;
+  uint16_t crc;
+
+  memset(&channel_info, 0, sizeof(channel_info));
+#if 0
+  while (channel_info.u.s.manufacturer_id != 0xa1) {
+    /*uart__getc(self->uart, (char*)&channel_info.u.s.manufacturer_id);*/
+#endif
+  while (channel_info.u1.s.manufacturer_id != 0xa1) {
+    uart__getc(self->uart, (char*)&channel_info.u1.s.manufacturer_id);
+  }
+  /* read the frame sent by the receiver and verify CRC16 checksum */
+  uart__read(self->uart, channel_info.u1.s.u.channels,
+             sizeof(struct jeti_udi__12_channel_info) - 1);
+#if 0
+
+  uart__read(self->uart, channel_info.u.s.channels,
+	     sizeof(struct jeti_udi__12_channel_info) -
+         sizeof(channel_info.u.s.manufacturer_id));
+#endif
+  // TODO: rework to use the lib-algorithms implementation
+  crc = 0xffff;
+  for(i = 0; i < 25; i++) {
+    crc = crc ^ channel_info.u1.payload[i] << 8;
+    for(j = 0; j < 8; j++) {
+      if(crc & 0x8000) {
+        crc = crc << 1 ^ 0x1021;
+      }
+      else {
+        crc = crc << 1;
+      }
+    }
+  }
+#if 0
+  if (crc == byteorder__be16_to_cpu(channel_info.crc16)) {
+#endif
+    /* Convert servo positions to native signed 16-bit format */
+    for (i = 0; i < JETI_UDI__12; i++) {
+      uint16_t channel = byteorder__be16_to_cpu(channel_info.u1.s.u
+                                                  .channels[i]);
+
+#if 0
+      int16_t channel = byteorder__be16_to_cpu(channel_info.u.s.channels[i]);
+
+      rc_channels__set(&servo_positions, i, (int16_t)channel) -;
+#endif
+      rc_channels__set(&servo_positions, i, ((int16_t)channel) - (int16_t)
+                                                                   0x800);
+#if 0
+    }
+#endif
+    /* post the servo positions for further processing or just mark it
+     * as dropped */
+    if (xQueueSend(self->servo_channels_queue, &servo_positions, 0) != pdPASS) {
+      self->drop_count++;
+    }
+
+  }
+}
+
+
+/**
+ * @todo this operation currently blocks indefinitely, however the
+ * implementation is ready for user specified timeout handling.
+ */
+int jeti_udi__read(struct jeti_udi *self, struct rc_channels *channels)
+{
+  int err = E_OK;
+  if (xQueueReceive(self->servo_channels_queue, channels,
+                    portMAX_DELAY) != pdPASS)
+    err = E_TIMEOUT;
+
+  return err;
+}
---
+++ lib-rc/jeti/jeti_udi.h
@@ -0,0 +1,171 @@
+/**
+ * @file jeti_udi.h
+ * @author Copyright (c) 2016 Braiins Systems s.r.o. All rights reserved.
+ * @brief JETI UDI12/16 RC channel data parser
+ */
+/**
+ * @defgroup group_jeti_udi Jeti UDI driver
+ * @brief Jeti UDI namespace
+ */
+#ifndef _LIB_RC_JETI_JETI_UDI_H_
+#define _LIB_RC_JETI_JETI_UDI_H_
+
+/* driver framework includes */
+#include <freertos-drivers/uart/uart.h>
+#include <freertos-drivers/gpio/gpio_pin.h>
+
+/* lib-rc includes */
+#include <lib-rc/rc_channels.h>
+
+#include <task.h>
+
+/**
+ * This class handles communication with JETI receivers that UDI12/16
+ * channel serial output.
+ *
+ *
+ * The data is sent @ 115200 8N1 rate in fixed frame sizes. The size
+ * of each frame contains is determined by the UDI protocol type - either
+ * 12 or 16
+ *
+ * @verbatim
+ * byte: meaning (
+ * ---------------------------
+ * 0: start byte (0xa1 - UDI12(12 channels), 0xa2 - UDI16 - 16 channels
+ * 1: MSB for channel 1
+ * 2: LSB for channel 1
+ * 3: MSB for channel 2
+ * 4: LSB for channel 2
+ * .
+ * .
+ *
+ * 23: MSB for channel 12
+ * 24: LSB for channel 12
+ * 25: CRC16 MSB -> polynom is 0x1021
+ * 26: CRC16 LSB
+ * --------------------------
+ * Total frame length: 27 bytes (31 bytes for UDI 16)
+ * @endverbatim
+ *
+ * The channel positions are sent as 12 bit values:
+ *
+ * 0x000 - min left/pull/low throttle
+ * 0x120 - -110 %
+ * 0x1C0 - -100 %
+ * 0x800 - 0 % - Center
+ * 0xE40 - +100 %
+ * 0xEE0 - +110 %
+ * 0xFFF - max right/push/high throttle
+ *
+ * @ingroup group_jeti_udi
+ */
+struct jeti_udi {
+  /** serial interface connected to the receiver */
+  struct uart *uart;
+
+  /** queue for communicating the servo positions to the reader. It is
+   * sufficient to have 1 element queue since buffering makes no sense
+   * here. If the reader is not fast enough to fetch the servo
+   * positions on time, something must be going wrong (e.g. broken RF
+   * link) */
+  QueueHandle_t servo_channels_queue;
+
+  /** statistics to keep track of how many servo position frames had
+   * to be dropped since the reader didn't fetch them */
+  unsigned long drop_count;
+};
+
+
+/**
+ * @ingroup group_jeti_udi
+ */
+typedef enum {
+  /** 12 RC channel mode */
+  JETI_UDI__12 = 12,
+  /** 16 RC channel mode */
+  JETI_UDI__16 = 16,
+} jeti_udi__format_t;
+
+
+/**
+ * Creates new instance and initializes communication with the
+ * receiver.
+ *
+ * @memberof jeti_udi
+ * @param uart_id - uart interface for communication with the jeti
+ * receiver
+ * @param udi_format - expected frame length see jeti_uid__format_t
+ *
+ * @return new instance or NULL upon failure
+ */
+struct jeti_udi* jeti_udi__new(uart__id_t uart_id,
+                               jeti_udi__format_t udi_format);
+
+
+/**
+ * Initializes communication with the receiver.
+ *
+ * @memberof jeti_udi
+ * @param *self - this jeti instance
+ * @param uart_id - uart interface for communication with the jeti
+ * receiver
+ * @param udi_format - expected frame length see jeti_uid__format_t
+ *
+ * @return E_OK when initialization has succeeded
+ */
+int jeti_udi__init(struct jeti_udi *self, uart__id_t uart_id,
+                   jeti_udi__format_t udi_format);
+
+
+/**
+ * This method handles incoming servo positions from the receiver,
+ * normalizes them and enqueues each frame for further processing.
+ *
+ * It should be called from a high priority task so that the
+ * synchronization with the receiver is never lost. Each set of
+ * servo positions is being enqueued. If it doesn't fit anymore into
+ * the queue it is simply dropped.
+ *
+ * @memberof jeti_udi
+ * @param *self - this jeti instance
+ */
+void jeti_udi__rx_task(struct jeti_udi *self);
+
+
+/**
+ * Blocking operation that waits until the receiver provides a new
+ * vector of servo channels.
+ *
+ * @memberof jeti_udi
+ * @param *self - this jeti instance
+ * @param *channels - output parameter for storing the received
+ * channels
+ */
+int jeti_udi__read(struct jeti_udi *self, struct rc_channels *channels);
+
+
+/**
+ * Resets the frame drop count.
+ *
+ * @memberof jeti_udi
+ * @param *self - this jeti instance
+ */
+static inline void jeti_udi__reset_drop_count(struct jeti_udi *self)
+{
+  self->drop_count = 0;
+}
+
+
+/**
+ * Drop count accessor.
+ *
+ * @memberof jeti_udi
+ * @param *self - this jeti instance
+ * @return current drop count value
+ */
+static inline unsigned long jeti_udi__get_drop_count(struct jeti_udi *self)
+{
+  return self->drop_count;
+}
+
+#endif /* _LIB_RC_JETI_JETI_UDI_H_ */
